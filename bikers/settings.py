5e4d763d70e63fcc927d05d35c07ab9d7960d157
Sachin Renjith <sachinranjith012gmail.com>
2023-07-14 15:35:44 +0530

    changes in settings

--- bikers/settings.py
+++ bikers/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'bikerapp',
     'accounts',
     'category',
@@ -58,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'bikers.urls'
--- bikers/urls.py
+++ bikers/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from django.conf import settings # new
+from  django.conf.urls.static import static #new
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('bikerapp.urls')),
@@ -25,3 +28,6 @@ urlpatterns = [
     path('cart/',include('carts.urls')),
     path('orderss/',include('orderss.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
