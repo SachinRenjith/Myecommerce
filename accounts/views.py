a48ca439ddddf756281a73f63a2d1607d2ccf29d
Sachin Renjith <sachinranjith012gmail.com>
2023-07-18 08:17:50 +0530

    cloudflare

--- accounts/views.py
+++ accounts/views.py
@@ -35,6 +35,7 @@ def register(request):
             username = form.cleaned_data['username']
             user = Account.objects.create_user(first_name = first_name, last_name = last_name, email = email, username = username, password = password )
             user.phone_number = phone_number
+        
             user.save()
            
             # USER ACTIVATION
