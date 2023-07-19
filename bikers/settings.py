490dfc50ca6ae51c593e0b72cbad333e7e749057
Sachin Renjith <sachinranjith012gmail.com>
2023-07-19 10:00:40 +0530

    true

--- bikers/settings.py
+++ bikers/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['13.53.146.180', 'bikerstore.online']
 
